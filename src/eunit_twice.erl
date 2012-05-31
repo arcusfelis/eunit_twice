63809decc45f6d847dc69cfca1ed46c6018fe8c7
Uvarov Michael <freeakk@gmail.com>
2012-05-31 17:16:49 +0400

    Second commit.

--- src/eunit_twice.erl
+++ src/eunit_twice.erl
@@ -2,5 +2,6 @@
 -include_lib("eunit/include/eunit.hrl").
 
 
-test_() ->
-    io:format(user, "Mod: ~s~n", [?MODULE_STRING]).
+first_test_() ->
+    io:format(user, "Mod: ~s~n", [?MODULE_STRING]),
+    [].
--- test/eunit_twice_tests.erl
+++ test/eunit_twice_tests.erl
@@ -1,5 +1,6 @@
 -module(eunit_twice_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-test_() ->
-    io:format(user, "Mod: ~s~n", [?MODULE_STRING]).
+first_test_() ->
+    io:format(user, "Mod: ~s~n", [?MODULE_STRING]),
+    [].
